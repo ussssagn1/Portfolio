d63d7ae4005a970aa1a9d99e87a778d9c4cea868
usssagn1 <zaytsevdima031107@gmail.com>
2023-10-12 18:54:02 +0300

    commit
    commit

---
+++ js/main.js
@@ -0,0 +1,179 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    Splitting();
+    luxy.init();
+    gsap.registerPlugin(ScrollTrigger);
+
+    const gTL = gsap.timeline();
+    gTL.from('.title .char', 1, {
+        opacity: 0,
+        yPercent: 130,
+        stagger: 0.06,
+        ease: 'black.out'
+    });
+    gTL.to('.header__img', 2, {
+        clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+        scale: 1,
+        ease: 'expo.out'
+    }, '-=1');
+    gTL.from('.header__marq', 1, {
+        opacity: 0,
+        yPercent: 100,
+        ease: 'expo.out'
+    }, '-=1.5');
+    const gsapSq = gsap.utils.toArray('.section-title__square');
+    gsapSq.forEach((gSq, i) => {
+        const rotat = gsap.from(gSq, 3, {rotation: 720});
+        ScrollTrigger.create({
+            trigger: gSq,
+            animation: rotat,
+            start: 'top bottom',
+            scrub: 1.9
+        })
+    })
+
+    //header 
+    function header() {
+        gsap.to('.title__paralax', {
+            scrollTrigger: {
+                trigger: '.header',
+                start: 'top top',
+                scrub: 1.9
+            },
+            yPercent: -150
+        })
+        gsap.to('.header .stroke', {
+            scrollTrigger: {
+                trigger: '.header',
+                start: 'top top',
+                scrub: 1.9
+            },
+            xPercent: 50
+        })
+        gsap.to('.header__img', {
+            scrollTrigger: {
+                trigger: '.header',
+                start: 'top top',
+                scrub: 1.9
+            },
+            xPercent: -70
+        })
+        gsap.to('.header__img img', {
+            scrollTrigger: {
+                trigger: '.header',
+                start: 'top top',
+                scrub: 1.9
+            },
+            scale: 1.3
+        })
+        gsap.to('.header__marq-wrapp', {
+            scrollTrigger: {
+                trigger: '.header',
+                start: 'top top',
+                scrub: 1.9
+            },
+            xPercent: -50
+        })
+        gsap.to('.header__marq-wrapp svg', {
+            scrollTrigger: {
+                trigger: '.header',
+                start: 'top top',
+                scrub: 1.9
+            },
+            rotate: -720
+        })
+    }
+    header()
+
+    //about
+    function about() {
+        gsap.to('.about__img', {
+            scrollTrigger: {
+                trigger: '.about',
+                start: 'top bottom',
+                scrub: 1.9
+            },
+            yPercent: -40
+        })
+        gsap.to('.about__img img', {
+            scrollTrigger: {
+                trigger: '.about',
+                start: 'top bottom',
+                scrub: 1.9
+            },
+            scale: 1.2
+        })
+        gsap.to('.about__text', {
+            scrollTrigger: {
+                trigger: '.about',
+                start: 'top bottom',
+                scrub: 1.9
+            },
+            yPercent: 50
+        })
+    }
+    about()
+
+    //benefits
+    function skills() {
+        gsap.from('.skills__num', {
+            x: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+            scrollTrigger: {
+                trigger: '.skills__list',
+                start: 'top bottom',
+                scrub: 1.9
+            }
+        })
+    }
+    skills()
+
+    //portfolio
+    function portfolio () {
+        gsap.from('.work__item, .work__item-num', {
+            y: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+            scrollTrigger: {
+                trigger: '.work',
+                start: 'top bottom',
+                scrub: 1.9
+            },
+        })
+        gsap.from('.work__item-img img', {
+            scale: 1.7,
+            scrollTrigger: {
+                trigger: '.work__wrapp',
+                start: 'top bottom',
+                scrub: 1.9
+            },
+        })
+        
+    }
+    portfolio()
+
+    //serv
+    function serv() {
+        gsap.from('.serv__item-arrow', {
+            x: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+            scrollTrigger: {
+                trigger: '.serv__list',
+                start: 'top bottom',
+                scrub: 1.9
+            }
+        })
+    }
+    serv()
+
+    //footer
+    function footer () {
+        gsap.from('.footer__div span', {
+            y: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+            opacity: 0,
+            scrollTrigger: {
+                trigger: '.footer',
+                start: 'top bottom',
+                end: 'bottom bottom',
+                scrub: 1.9
+            }
+        })
+    }
+    footer()
+})
